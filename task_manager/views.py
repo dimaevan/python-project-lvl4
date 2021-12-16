0b97fea2e92301660ca899805549c72a264543b5
Dima Evan <dimaevan@gmail.com>
2021-12-16 14:02:21 +0300

    Part 3. Edit users

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,9 +1,8 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
-# from django.http import HttpResponse
-from django.contrib.auth import authenticate, login, logout
-from .forms import UserForm
+from django.contrib.auth import authenticate, login, logout, get_user
+from .forms import UserForm, RegistrationForm
 
 
 class MainView(TemplateView):
@@ -36,20 +35,14 @@ def logout_view(request):
 
 
 def registration(request):
-    if request.method == 'GET':
-        return render(request, 'registration.html')
     if request.method == 'POST':
-        name = request.POST['username']
-        surname = request.POST['surname']
-        password = request.POST['password']
-        password2 = request.POST['password2']
-        nickname = request.POST['nickname']
-
-        if password == password2:
-            user = User.objects.create_user(username=nickname, email=None, password=password, first_name=name,
-                                            last_name=surname)
-            user.save()
-        return redirect('main_page')
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main_page')
+    else:
+        form = RegistrationForm()
+        return render(request, 'registration.html', {'form': form})
 
 
 def delete_user(request):
@@ -57,15 +50,15 @@ def delete_user(request):
 
 
 def update_user(request, user_id):
-    user = get_object_or_404(User, pk=user_id)
-
+    this_user = get_object_or_404(User, pk=user_id)
+    login_user = get_user(request).pk
     if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)
-        if form.is_valid():
+        form = UserForm(request.POST, instance=request.user)
+        if form.is_valid() and this_user.pk == login_user:
             user = form.save()
             user.save()
-            return redirect('update_user', user_id=user.pk)
+            return redirect('update_user', user_id=request.user.pk)
     else:
-        form = UserForm(instance=user)
+        form = UserForm(instance=this_user)
 
-    return render(request, 'user.html', {'form': form})
+    return render(request, 'user.html', {'form': form, 'log_user': login_user, 'this_user': this_user.pk})
