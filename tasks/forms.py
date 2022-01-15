0fa9c2f0992575ca60344d12aa01a9aab112d6a3
dimaevan <dimaevan@gmail.com>
2022-01-15 11:51:22 +0300

    Add GA lint & test. Fix it.

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,7 +5,6 @@ from . models import Task
 class TaskForm(forms.ModelForm):
     description = forms.CharField(widget=forms.Textarea(attrs={'rows': 2}))
 
-
     class Meta:
         model = Task
         exclude = ('author',)
