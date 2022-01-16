e6a0c72858bca021cdaed23ad6ffe8534007257d
dimaevan <dimaevan@gmail.com>
2022-01-16 12:30:03 +0300

    Add DB settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import dotenv
 import os
+import dj_database_url
 
 # config = dotenv.dotenv_values('local.env')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -11,8 +12,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+dotenv_file = os.path.join(BASE_DIR, 'local.env')
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv('local.env')
 
-dotenv.load_dotenv('local.env')
+
 SECRET_KEY = os.environ['SECRET']
 DEBUG = os.environ['DEBUG']
 ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
@@ -73,12 +77,15 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
@@ -115,10 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
