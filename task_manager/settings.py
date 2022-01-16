e568821a4fc4a12f949169035908c3b4786168ef
dimaevan <dimaevan@gmail.com>
2022-01-16 17:50:22 +0300

    Edit settings/3

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -3,24 +3,19 @@ import dotenv
 import os
 import dj_database_url
 
-# config = dotenv.dotenv_values('local.env')
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEBUG = False
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 dotenv_file = os.path.join(BASE_DIR, 'local.env')
+
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv('local.env')
+    DEBUG = True
 
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG')
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
-
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -78,16 +73,7 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {}
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -139,7 +125,7 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale'), ]
 
 
 ROLLBAR = {
-    'access_token': os.getenv('Rollbar_access_token'),
+    'access_token': os.getenv('ROLLBAR_ACCESS_TOKEN'),
     'environment': 'production',
     'branch': 'master',
     'root': os.getcwd(),
