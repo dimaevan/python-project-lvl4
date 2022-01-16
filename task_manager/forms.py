b7c9db2abb9c97d390d0b52b1d8429565a22e9d5
dimaevan <dimaevan@gmail.com>
2022-01-16 14:26:04 +0300

    Edit User forms

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -5,24 +5,18 @@ from django import forms
 
 
 class UserUpdateForm(UserCreationForm):
-    # Texts:
-
-    text_help_username = _('Required field. No more than 150 characters. Only letters, numbers and '
-                           '@/./+/-/_ characters.')
-    text_first_name = _('First name')
-    text_last_name = _('Last name')
-    text_password = _('Password')
-
-    username = forms.CharField(max_length=100, label=_('User'),
-                               help_text=text_help_username)
-    first_name = forms.CharField(max_length=100, label=text_first_name, required=False)
-    last_name = forms.CharField(max_length=100, label=text_last_name, required=False)
-    password1 = forms.CharField(max_length=100, label=text_password)
-    password2 = forms.CharField(max_length=100,
-                                label=_('Password confirm'), help_text=_('Please enter your password again '
-                                                                         'to confirm.'))
+    # username = forms.CharField(max_length=100, label='User',
+    #                            help_text=_('Required field. No more than 150 characters. Only letters, numbers and '
+    #                                        '@/./+/-/_ characters.'))
+    # first_name = forms.CharField(max_length=100, label=_('First name'), required=False)
+    # last_name = forms.CharField(max_length=100, label=_('Last name'), required=False)
+    # password1 = forms.CharField(max_length=100, label=_('Password'))
+    # password2 = forms.CharField(max_length=100,
+    #                             label=_('Password confirm'), help_text=_('Please enter your password again '
+    #                                                                      'to confirm.'))
 
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'password1',
                   'password2']
+
