c53b1b9376c80c68c54361953dcc3fcaf2b4e9b2
dimaevan <dimaevan@gmail.com>
2022-01-16 14:01:48 +0300

    Change nav.html and delete staticfiles

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/4438aa61d7196936be8a/maintainability)](https://codeclimate.com/github/dimaevan/python-project-lvl4/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/4438aa61d7196936be8a/test_coverage)](https://codeclimate.com/github/dimaevan/python-project-lvl4/test_coverage)
 
-[Link to site](https://desolate-waters-66248.herokuapp.com/)
+[Link to site](https://hexlet-lvl4-project.herokuapp.com/)
--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,16 +1,23 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 from django import forms
 
 
 class UserUpdateForm(UserCreationForm):
-    username = forms.CharField(max_length=100, label='User',
-                               help_text=_('Required field. No more than 150 characters. Only letters, numbers and '
-                                           '@/./+/-/_ characters.'))
-    first_name = forms.CharField(max_length=100, label=_('First name'), required=False)
-    last_name = forms.CharField(max_length=100, label=_('Last name'), required=False)
-    password1 = forms.CharField(max_length=100, label=_('Password'))
+    # Texts:
+
+    text_help_username = _('Required field. No more than 150 characters. Only letters, numbers and '
+                           '@/./+/-/_ characters.')
+    text_first_name = _('First name')
+    text_last_name = _('Last name')
+    text_password = _('Password')
+
+    username = forms.CharField(max_length=100, label=_('User'),
+                               help_text=text_help_username)
+    first_name = forms.CharField(max_length=100, label=text_first_name, required=False)
+    last_name = forms.CharField(max_length=100, label=text_last_name, required=False)
+    password1 = forms.CharField(max_length=100, label=text_password)
     password2 = forms.CharField(max_length=100,
                                 label=_('Password confirm'), help_text=_('Please enter your password again '
                                                                          'to confirm.'))
