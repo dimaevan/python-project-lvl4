f4edc44faac966cf04076fb7e51dc7beea960181
dimaevan <dimaevan@gmail.com>
2022-01-17 17:08:10 +0300

    Tranlations/13/Add delete user flash

--- statuses/forms.py
+++ statuses/forms.py
@@ -5,4 +5,4 @@ from django.forms import ModelForm
 class StatusForm(ModelForm):
     class Meta:
         model = Status
-        fields = ['status']
+        fields = ['name',]
--- statuses/models.py
+++ statuses/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.utils.translation import gettext as _
+from datetime import datetime
 
 
 class Status(models.Model):
-    status = models.CharField(max_length=100, verbose_name=_('Name'))
-    created = models.DateTimeField(auto_now_add=True, verbose_name=_('Data creation'))
+    name = models.CharField(max_length=100, verbose_name=_('Name'))
+    created = models.DateTimeField(verbose_name=_('Data creation'), auto_now_add=True)
 
     class Meta:
         verbose_name_plural = "Status"
 
     def __str__(self):
-        return self.status
+        return self.name
