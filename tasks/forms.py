8715b10de5a96ace72e5685456272cca98f6af02
dimaevan <dimaevan@gmail.com>
2022-01-17 20:06:09 +0300

    Tranlations/18/

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,9 +1,18 @@
 from django import forms
 from . models import Task
+from django.contrib.auth.models import User
+
+
+class UserModelChoiceField(forms.ModelChoiceField):
+
+    def label_from_instance(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
 
 
 class TaskForm(forms.ModelForm):
+    executor = UserModelChoiceField(User.objects.all())
 
     class Meta:
         model = Task
         exclude = ('author',)
+
