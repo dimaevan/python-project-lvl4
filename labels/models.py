729efb859dd9bc83102a1c62f7957ae270daf6d6
dimaevan <dimaevan@gmail.com>
2022-01-17 14:21:07 +0300

    Translations/3

--- labels/models.py
+++ labels/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext as _
 
 
 class Label(models.Model):
-    name = models.CharField(max_length=100, verbose_name=_('Label'))
+    name = models.CharField(max_length=100, verbose_name=_('Name'))
     created = models.DateTimeField(auto_now_add=True, verbose_name=_('Data creation'))
 
     def __str__(self):
--- statuses/views.py
+++ statuses/views.py
@@ -3,6 +3,10 @@ from .models import Status
 from .forms import StatusForm
 from django.urls import reverse_lazy
 from task_manager import mixins
+from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
+from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
 
 
 class StatusesListView(mixins.MyLoginRequired, ListView):
@@ -14,7 +18,12 @@ class StatusCreateView(mixins.MyLoginRequired, CreateView):
     model = Status
     form_class = StatusForm
     template_name = 'statuses/statuses_add.html'
-    success_url = reverse_lazy('statuses')
+
+    def form_valid(self, form):
+        text = _('Status successfully created ')
+        messages.add_message(self.request, messages.SUCCESS, text)
+        form.save()
+        return redirect('statuses')
 
 
 class StatusDetailView(mixins.MyLoginRequired, UpdateView):
@@ -28,3 +37,19 @@ class StatusDeleteView(mixins.MyLoginRequired, DeleteView):
     model = Status
     template_name = 'statuses/status_delete.html'
     success_url = reverse_lazy('statuses')
+    text_success = _('Status successfully deleted')
+    text_error = _('It is impossible to delete a status because it is in use ')
+
+    def get_object(self, queryset=None):
+        obj = super(StatusDeleteView, self).get_object()
+        return obj
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object.task_set.count() != 0:
+            messages.add_message(self.request, messages.ERROR, self.text_error)
+            return redirect('statuses')
+        success_url = self.get_success_url()
+        self.object.delete()
+        messages.add_message(self.request, messages.INFO, self.text_success)
+        return HttpResponseRedirect(success_url)
